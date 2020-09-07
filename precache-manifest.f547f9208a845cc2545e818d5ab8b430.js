4f35670adc2b4bf267df504bdb9fca70fca031a9
Mariana Barros <mamirandabarros11@gmail.com>
2020-09-06 22:46:12 -0300

    Updates

---
+++ precache-manifest.f547f9208a845cc2545e818d5ab8b430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ae498908a0084701939ffbd6d708aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "365cc5f58e538f0cee49",
+    "url": "/static/css/main.8bb345cb.chunk.css"
+  },
+  {
+    "revision": "c7cb6a3b6f6eda4936c0",
+    "url": "/static/js/2.ec86635f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec86635f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365cc5f58e538f0cee49",
+    "url": "/static/js/main.f0c6e61a.chunk.js"
+  },
+  {
+    "revision": "d30514b24cae20df9bf2",
+    "url": "/static/js/runtime-main.86f42d4f.js"
+  }
+]);
